f1fd6722d431db325f9d84acd13545138175967e
Malith-nethsiri <malithvihanga96@gmail.com>
2025-05-19 00:16:42 +0530

    Add files via upload
    little programs while im learning the python

---
+++ shopping cart program.py
@@ -0,0 +1,26 @@
+# shopping cart program
+
+foods = []
+prices =[]
+total = 0
+
+while True :
+    food = input("what is the food (press q to quit) -- ")
+    if food.lower() == "q" :
+        break
+    else:
+        price = float(input(f"what is the price of {food} -- $"))
+        foods.append(food)
+        prices.append(price)
+
+print("----- the bill -----")
+
+for food in foods:
+    print(food)
+
+for price in prices:
+    total += price
+
+print(f"the total is ${total}")
+
+
